0632032365bcc63089eda24835b65b9a17e43ce8
Shinzuu <not.shinzuu@gmail.com>
2024-04-08 02:19:00 +0600

    forgot about DOM! tehee

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,21 +1,47 @@
 let player = 0;
 let computer = 0;
 
+// Get reference to the div element with class "score"
+const scoreElement = document.querySelector(".score");
+
 // Get references to all buttons
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const result = startRound(button.id);
-        console.log(result);
+        //round score new line
+        const paragraph = document.createElement("p");
+        paragraph.textContent = result;
+        scoreElement.appendChild(paragraph);
 
         // Check if a player or computer has reached 5 points
         if (player === 5 || computer === 5) {
             const gameResult = determineWinner();
-            console.log(gameResult);
+
+            const finalResult = document.createElement("h1");
+            finalResult.textContent = gameResult;            // Append the final result paragraph to the score element
+            scoreElement.appendChild(finalResult);
 
             // Reset scores
             player = 0;
             computer = 0;
+            // Make buttons unclickable
+            buttons.forEach(button => {
+                button.disabled = true;
+            })
+            // Add a "Play Again" button
+            const playAgainButton = document.createElement("button");
+            playAgainButton.textContent = "Play Again";
+
+            playAgainButton.addEventListener("click", () => {
+                // Remove all child elements from the score element
+                scoreElement.innerHTML = "";
+                // Make buttons clickable
+                buttons.forEach(button => {
+                    button.disabled = false;
+                })
+            });
+            scoreElement.appendChild(playAgainButton);
         }
     });
 });
